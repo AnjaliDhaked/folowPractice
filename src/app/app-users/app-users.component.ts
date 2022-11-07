f28f468bf92b1f6e99e8514f6c3953afda28859e
MoinShaikh611 <imoin611@gmail.com>
2022-11-07 08:12:40 +0530

    grid view list view tab

--- src/app/app-users/app-users.component.ts
+++ src/app/app-users/app-users.component.ts
@@ -3,13 +3,21 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-app-users',
   templateUrl: './app-users.component.html',
-  styleUrls: ['./app-users.component.css']
+  styleUrls: ['./app-users.component.css'],
 })
 export class AppUsersComponent implements OnInit {
+  listViewActive = true;
+  gridViewActive = false;
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  listViewTab() {
+    this.listViewActive = true;
+    this.gridViewActive = false;
   }
-
+  gridViewTab() {
+    this.listViewActive = false;
+    this.gridViewActive = true;
+  }
 }
